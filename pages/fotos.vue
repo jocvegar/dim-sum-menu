bebeffcdf05bde72b864abc31e752e29e67ab69d
Jose <jocvegar@gmail.com>
2020-06-22 15:55:55 -0600

    Adds customer nerfle

--- pages/fotos.vue
+++ pages/fotos.vue
@@ -1,9 +1,9 @@
 <template>
         <div>
-            <h1>HOLA</h1>
-            <div>
+            <div class="relative h-screen"> 
                 <client-only>
-                    <slideshow 
+                    <component
+                        :is="slideshow"
                         :images="images"
                         :collageSizeMin="5"
                         :collageSizeMax="5"
@@ -11,29 +11,45 @@
                         :showNoImagesMsg="true"
                         noImagesMsg="No Images"
                         :showLoadingMsg="true"
-                        loadingMsg="Loading..."
-                        height="70%">
-                    </slideshow> 
+                        loadingMsg="Cargando..."
+                        height="100%"
+                        class="absolute">
+                    </component> 
                 </client-only>
             </div>
+            <section class="footer">
+                <div class="grid grid-cols-1">
+                <footer class="flex flex-col items-center flex-no-shrink justify-center bg-brown-800 h-24 text-center">
+                <p class="text-center text-lg text-white">Mandanos tus fotos!</p>
+                <p class="text-center text-lg text-white">jocvegar@gmail.com</p>
+                </footer>
+                </div>
+            </section>
         </div>
 </template>
 
 <script>
 export default {
     components: {
-        Slideshow: async () => await import('vue-collage-slideshow')
+        // Slideshow: () => import('vue-collage-slideshow')
+    },
+    computed: {
+        slideshow() {
+            return () => {
+                if (process.client) {
+                    return import('vue-collage-slideshow')
+                }
+            }
+        }
     },
     data() {
         return {
             images: [
                 {image: "https://wallpapershome.com/images/pages/pic_v/5111.jpg"},
                 {image: "https://thumbor.forbes.com/thumbor/1280x868/https%3A%2F%2Fblogs-images.forbes.com%2Fannabel%2Ffiles%2F2018%2F02%2FLouisville_Skyline-1200x801.jpg"},
-                {image: "https://t-ec.bstatic.com/images/hotel/max1024x768/683/68345284.jpg"},
                 {image: "https://i.pinimg.com/originals/08/ec/94/08ec94ecf38048a2102ec4783dc88fa8.jpg"},
                 {image: "https://t-ec.bstatic.com/images/hotel/max1024x768/873/87316855.jpg"},
                 {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
-                {image: "http://nexusandme.com/wp-content/uploads/2014/06/Ferris-Wheel.jpg"},
                 {image: "http://unmaskmycity.org/wp-content/uploads/2017/01/cityneighborhood.jpg"},
                 {image: "https://wallpapershome.com/images/pages/pic_v/660.jpg"},
                 {image: "http://www.manager.bg/sites/default/files/mainimages/20aa_29.jpg"},
@@ -43,15 +59,12 @@ export default {
                 {image: "https://img4.goodfon.com/wallpaper/nbig/b/d1/skyscrapers-building-architecture-vertical-night-lights-city.jpg"},
                 {image: "https://images.wallpaperscraft.com/image/architecture_city_view_from_above_buildings_river_118446_1920x1080.jpg"},
                 {image: "https://images.unsplash.com/photo-1534856966153-c86d43d53fe0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
-                {image: "https://images.wallpaperscraft.com/image/architecture_city_beautiful_building_sky_59431_1920x1080.jpg"},
                 {image: "https://images.unsplash.com/photo-1541780815498-ffd0f2d0d0af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
                 {image: "https://images.unsplash.com/photo-1536235799634-c6c21fdc2b9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
-                {image: "https://data.whicdn.com/images/126024359/large.jpg"},
                 {image: "https://images.wallpaperscraft.com/image/bridge_night_city_lighting_125945_1280x720.jpg"},
                 {image: "https://images.unsplash.com/photo-1501464915526-070295e4768d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
                 {image: "https://images.wallpaperscraft.com/image/architecture_building_sky_114682_1920x1080.jpg"},
                 {image: "https://images.wallpaperscraft.com/image/bridge_night_city_tokyo_120902_1280x720.jpg"},
-                {image: "https://images.wallpaperscraft.com/image/trolley_urban_landscape_san_diego_california_usa_50544_1280x720.jpg"},
                 {image: "https://images.unsplash.com/photo-1541780050666-f64773a3d779?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
                 {image: "https://images.wallpaperscraft.com/image/bridge_night_city_bay_124832_1280x720.jpg"},
                 {image: "https://images.unsplash.com/photo-1519359810479-37e924ddd0b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
@@ -72,6 +85,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.footer {
+    font-family: 'Montserrat', sans-serif;
+}
 </style>
