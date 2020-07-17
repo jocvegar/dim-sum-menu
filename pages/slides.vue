f70cf4dfc6e03fc25d4f7b4bdab0f0a6f1576b61
Jose <jocvegar@gmail.com>
2020-07-17 13:53:39 -0600

    Adds vueper slides

--- pages/slides.vue
+++ pages/slides.vue
@@ -1,33 +1,73 @@
 <template>
-    <hooper 
-       :centerMode="true" 
-       pagination="no"
-       :progress="true" 
-       :autoPlay="true" 
-       :playSpeed="2000"
-       :infiniteScroll="true"
-    >
-        <slide v-for="(slide, indx) in slides" :key="indx" :index="indx">
-            <img :src=slide.image>
-        </slide>
-    </hooper>
+    <div>
+        <client-only>
+            <vueper-slides 
+                autoplay
+                class="no-shadow"
+                :visible-slides="3"
+                slide-multiple
+                :infinite="true" 
+                :arrows="false"
+                :bullets="false"
+                :gap="1"
+                progress
+                lazy 
+                :slide-ratio="1 / 3"
+                :dragging-distance="70"
+                :breakpoints="{ 800: { 
+                    visibleSlides: 1, 
+                    slideRatio: 1,
+                    } 
+                }">
+                <vueper-slide
+                    v-for="(slide, i) in slides"
+                    :key="i"
+                    :image="require(`~/assets/images/image-${slide.image}.jpg`)"/>
+            </vueper-slides>
+        </client-only>
+
+        <div class="my-8">
+            <h1 class="text-brown-400 text-xl text-center font-medium uppercase">
+                Gracias por las fotos!
+            </h1>
+        </div>
+    </div>
 </template>
 
 <script>
-import { Hooper, Slide } from 'hooper';
-import 'hooper/dist/hooper.css';
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
 
 export default {
-    name: 'App',
-    components: { Hooper, Slide },
+    components: { VueperSlides, VueperSlide },
     data: () => ({
+        primarySrc: '',
+        pauseOnHover: true,
+        autoPlaying: true,
         slides: [
-            { image: require('~/assets/images/image-1.jpg') },
-            { image: require('~/assets/images/image-2.jpg') },
-            { image: require('~/assets/images/image-3.jpg') },
-            { image: require('~/assets/images/image-4.jpg') },
-            { image: require('~/assets/images/image-5.jpg') },
-        ]    
+            { image: 1 },
+            { image: 2 },
+            { image: 3 },
+            { image: 4 },
+            { image: 5 },
+            { image: 6 },
+            { image: 7 },
+            { image: 8 },
+            { image: 9 },
+            { image: 10 },
+            { image: 11 },
+            { image: 12 },
+            { image: 13 },
+            { image: 14 },
+            { image: 15 },
+
+        ]
     })
-};
+}
 </script>
+
+<style>
+h1 {
+  font-family: 'Sriracha', cursive;
+}
+</style>
