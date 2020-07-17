bc0efe9645ca21bd874bb519155bddaf9da19669
Jose <jocvegar@gmail.com>
2020-07-17 09:52:26 -0600

    adds more pictures

--- pages/fotos.vue
+++ pages/fotos.vue
@@ -1,31 +1,31 @@
 <template>
-        <div>
-            <div class="relative h-screen"> 
-                <client-only>
-                    <component
-                        :is="slideshow"
-                        :images="images"
-                        :collageSizeMin="2"
-                        :collageSizeMax="3"
-                        :slidesInterval="5000"
-                        :showNoImagesMsg="true"
-                        noImagesMsg="No Images"
-                        :showLoadingMsg="true"
-                        loadingMsg="Cargando..."
-                        height="100%"
-                        class="absolute">
-                    </component> 
-                </client-only>
-            </div>
-            <section class="footer">
-                <div class="grid grid-cols-1">
-                <footer class="flex flex-col items-center flex-no-shrink justify-center bg-brown-800 h-24 text-center">
-                <p class="text-center text-lg text-white">Mandanos tus fotos!</p>
-                <p class="text-center text-lg text-white">jocvegar@gmail.com</p>
-                </footer>
-                </div>
-            </section>
-        </div>
+  <div>
+      <div class="relative h-screen"> 
+          <client-only>
+              <component
+                  :is="slideshow"
+                  :images="images"
+                  :collageSizeMin="2"
+                  :collageSizeMax="3"
+                  :slidesInterval="5000"
+                  :showNoImagesMsg="true"
+                  noImagesMsg="No Images"
+                  :showLoadingMsg="true"
+                  loadingMsg="Cargando..."
+                  height="100%"
+                  class="absolute">
+              </component> 
+          </client-only>
+      </div>
+      <section class="footer">
+          <div class="grid grid-cols-1">
+          <footer class="flex flex-col items-center flex-no-shrink justify-center bg-brown-800 h-24 text-center">
+          <p class="text-center text-lg text-white">Mandanos tus fotos!</p>
+          <p class="text-center text-lg text-white">jocvegar@gmail.com</p>
+          </footer>
+          </div>
+      </section>
+  </div>
 </template>
 
 <script>
@@ -67,6 +67,19 @@ export default {
         { image: require('~/assets/images/image-20.jpg') },
         { image: require('~/assets/images/image-21.jpg') },
         { image: require('~/assets/images/image-22.jpg') },
+        { image: require('~/assets/images/image-23.jpg') },
+        { image: require('~/assets/images/image-24.jpg') },
+        { image: require('~/assets/images/image-25.jpg') },
+        { image: require('~/assets/images/image-26.jpg') },
+        { image: require('~/assets/images/image-27.jpg') },
+        { image: require('~/assets/images/image-28.jpg') },
+        { image: require('~/assets/images/image-29.jpg') },
+        { image: require('~/assets/images/image-30.jpg') },
+        { image: require('~/assets/images/image-31.jpg') },
+        { image: require('~/assets/images/image-32.jpg') },
+        { image: require('~/assets/images/image-33.jpg') },
+        { image: require('~/assets/images/image-34.jpg') },
+        { image: require('~/assets/images/image-35.jpg') },
       ]
     }
   }
